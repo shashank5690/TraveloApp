59ba18d16233905bc9f19007dec28face1b0085e
Shashank singh <sshubham.5690@gmail.com>
2024-08-04 16:23:46 +0530

    Added the styles in card

--- src/Screens/ScreenHome/components/Card.tsx
+++ src/Screens/ScreenHome/components/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { Destination } from '../utils/Interface';
+import styles from './styleCard';
 
 interface CardProps {
   item: Destination;
@@ -21,63 +22,6 @@ const Card: React.FC<CardProps> = ({ item }) => (
   </TouchableOpacity>
 );
 
-const styles = StyleSheet.create({
-  card: {
-    width: '95%',
-    marginBottom: 16,
-    borderRadius: 10,
-    overflow: 'hidden',
-    backgroundColor: '#fff',
-    shadowColor: '#8d99ae',
-    shadowOffset: { width: 3, height: 5 },
-    shadowOpacity: 0.6,
-    shadowRadius: 6,
-    elevation: 12,
-    marginHorizontal: 12,
-  },
-  image: {
-    width: '100%',
-    height: 200,
-  },
-  cardContent: {
-    padding: 16,
-  },
-  nameHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 8,
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  country: {
-    fontSize: 18,
-    color: '#FF7029',
-    fontWeight: 'bold',
-  },
-  infoRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 8,
-  },
-  language: {
-    fontSize: 14,
-    color: '#000',
-  },
-  topAttractions: {
-    fontSize: 16,
-    color: '#000',
-    textAlign: 'right',
-    fontWeight: 'bold',
-  },
-  description: {
-    fontSize: 12,
-    color: '#555',
-    marginTop: 8,
-  },
-});
+
 
 export default Card;
