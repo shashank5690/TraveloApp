e4de466f1995a3aa7ca4699c65c7f61ed02f74cd
Shashank singh <shashank.singh@binmile.com>
2024-08-05 15:49:47 +0530

    Fixed homescreen

--- src/Screens/ScreenHome/components/Card.tsx
+++ src/Screens/ScreenHome/components/Card.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { Destination } from '../utils/Interface';
 import styles from './styleCard';
+import Rating from '../Assets/rating';
+import Location from '../Assets/location'; // Import the 'Loaction' component
 
 interface CardProps {
   item: Destination;
@@ -13,15 +15,14 @@ const Card: React.FC<CardProps> = ({ item }) => (
     <View style={styles.cardContent}>
       <View style={styles.nameHeader}>
         <Text style={styles.title}>{item.name}</Text>
-        <Text style={styles.country}>{item.country}</Text>
+        <Text style={styles.country}>📍({item.country})</Text>
+        <Rating height={55} width={50} style={styles.Rating}/>
       </View>
       <View style={styles.infoRow}>
-        <Text style={styles.topAttractions}>Top Attractions: {item.top_attractions.slice(0, 2).join(', ')}</Text>
+       <Location height={20} width={20} style={styles.Location}/>
+        <Text style={styles.topAttractions}>Top Places: {item.top_attractions.slice(0, 2).join(', ')}</Text>
       </View>
     </View>
   </TouchableOpacity>
 );
-
-
-
 export default Card;
